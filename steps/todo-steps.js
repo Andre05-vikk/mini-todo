e86221fb01ba7a7fa13e63e1138d9b8793da938f
Andre <andrepark05@gmail.com>
2025-11-20 16:02:39 +0200

    green: complete todo by title and getByTitle method

--- steps/todo-steps.js
+++ steps/todo-steps.js
@@ -25,3 +25,19 @@ Then('item {int} status is {string}', function (index, status) {
   const todo = todoList.getByIndex(index - 1);
   assert.equal(todo.status, status);
 });
+
+Given('the list contains a todo {string}', function (title) {
+  if (!todoList) {
+    todoList = new TodoList();
+  }
+  todoList.add(title);
+});
+
+When('I mark {string} as completed', function (title) {
+  todoList.complete(title);
+});
+
+Then('{string} status is {string}', function (title, status) {
+  const todo = todoList.getByTitle(title);
+  assert.equal(todo.status, status);
+});
